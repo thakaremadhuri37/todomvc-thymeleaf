890dd094f15bb0628c04040b44828043bd28b52d
thakaremadhuri37 <101014899+thakaremadhuri37@users.noreply.github.com>
2022-06-06 16:29:19 +0530

    Delete todomvc-thymeleaf/src directory

--- todomvc-thymeleaf/src/main/java/com/wimdeblauwe/examples/todomvcthymeleaf/todoitem/TodoItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wimdeblauwe.examples.todomvcthymeleaf.todoitem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
- 
-}
